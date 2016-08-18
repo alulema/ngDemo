48c49fe5e28f6ce8e78bdb70758f44dcae9f267f
Alexis <alexis.alulema@gmail.com>
2016-08-18 15:16:13 -0500

    Ok

--- app/mainScript.js
+++ app/mainScript.js
@@ -6,17 +6,25 @@
     var app = angular.module("githubViewer", []);
 
     var MainController = function($scope, $http) {
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        };
+
         var onFullfillment = function (response) {
             $scope.user = response.data;
+            $http.get($scope.user.repos_url).then(onRepos, onRejection);
         };
 
         var onRejection = function (response) {
             $scope.error = "Could not fetch the user";
         };
 
-        $http.get("https://api.github.com/users/robconery").then(onFullfillment, onRejection);
+        $scope.search = function (username) {
+            $http.get("https://api.github.com/users/" + username).then(onFullfillment, onRejection);
+        };
 
-        $scope.message = "Hello Angular!";
+        $scope.message = "GitHub viewer!";
+        $scope.username = "angular";
     };
 
     app.controller("MainController", ["$scope", "$http", MainController]);
