e6450365e80a648b203992ff0f547eb75df0bd18
Alexis Alulema <alexis.alulema@gmail.com>
2016-08-19 15:35:08 -0500

    Ok

--- app/mainScript.js
+++ app/mainScript.js
@@ -5,9 +5,11 @@
 (function () {
     var app = angular.module("githubViewer", []);
 
-    var MainController = function($scope, $http) {
+    var MainController = function($scope, $http, $interval, $log, $anchorScroll, $location) {
         var onRepos = function (response) {
             $scope.repos = response.data;
+            $location.hash("userDetails");
+            $anchorScroll();
         };
 
         var onFullfillment = function (response) {
@@ -19,14 +21,37 @@
             $scope.error = "Could not fetch the user";
         };
 
+        var decrementCountDown = function () {
+            $scope.countdown -= 1;
+
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+
+        var startCountDown = function () {
+            countdownInterval = $interval(decrementCountDown, 1000, $scope.countdown);
+        };
+
         $scope.search = function (username) {
+            $log.info("Searching for " + username);
             $http.get("https://api.github.com/users/" + username).then(onFullfillment, onRejection);
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
         };
 
         $scope.message = "GitHub viewer!";
         $scope.username = "angular";
+        $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+
+        startCountDown();
     };
 
-    app.controller("MainController", ["$scope", "$http", MainController]);
+    app.controller("MainController", ["$scope", "$http", "$interval", "$log", "$anchorScroll", "$location", MainController]);
 }());
 
---
+++ github.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Alexis on 8/19/2016.
+ */
+
+(function () {
+    var github = function ($http) {
+        var getUser = function (username) {
+            return $http.get("https://api.github.com/users/" + username)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        return {
+
+        };
+    };
+
+    var module = angular.module("githubViewer");
+}());
